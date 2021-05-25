60fffd499c51814d5e118bfdbaffad9bf78ddf41
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-05-24 22:44:10 -0300

    Adicinando persistencia

---
+++ DeliveryCafe.API/DeliveryCafe.API/Persistence/DeliveryContext.cs
@@ -0,0 +1,21 @@
+﻿using DeliveryCafe.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryCafe.API.Persistence
+{
+    public class DeliveryContext : DbContext
+    {
+        public DeliveryContext(DbContextOptions<DeliveryContext> options ) : base(options)
+        {
+                
+        }
+
+        public DbSet<Usuario> Usuarios { get; set; }
+        public DbSet<Endereco> Enderecos { get; set; }
+
+    }
+}
