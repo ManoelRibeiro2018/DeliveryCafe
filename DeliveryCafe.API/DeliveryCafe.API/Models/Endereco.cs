abadef8dfe115c9b968fd9d3f65b40cc942ce030
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-05-24 22:28:47 -0300

    Criando classes de dominio

---
+++ DeliveryCafe.API/DeliveryCafe.API/Models/Endereco.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryCafe.Models
+{
+    public class Endereco
+    {
+        public int Id { get; set; }
+        public string CEP { get; set; }
+        public string Logadouro { get; set; }
+        public string Cidade { get; set; }
+        public string Bairro { get; set; }
+        public int IdUsuario { get; set; }
+        public Usuario Usuario { get; set; }
+    }
+}
