244bd42c988b2fff0c5b48c7a966c9285e70fa72
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-06-01 16:20:26 -0300

    Contrato de produto

---
+++ DeliveryCafe.API/DeliveryCafe.API/Interface/Domain/IProdutoInterface.cs
@@ -0,0 +1,17 @@
+﻿using DeliveryCafe.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryCafe.API.Interface.Domain
+{
+    public interface IProdutoInterface
+    {
+        Produto Insert(Produto produto);
+        bool Update(int id, Produto produto);
+        bool Delete(int id);
+        Produto GetById(int id);
+        List<Produto> GetAll();
+    }
+}
