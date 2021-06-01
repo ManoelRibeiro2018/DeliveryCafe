b1645a4f1b1f7420e6af1eb6b417f1ab4e0b63c2
Manoel Ribeiro <manoelribeiro20181@gmail.com>
2021-05-31 21:49:42 -0300

    Implementando padrão Service para Endereco

---
+++ DeliveryCafe.API/DeliveryCafe.API/Interface/DTO/IEnderecoDTOInterface.cs
@@ -0,0 +1,15 @@
+﻿using DeliveryCafe.API.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryCafe.API.Interface.DTO
+{
+    public interface IEnderecoDTOInterface
+    {
+        EnderecoDTO Insert(EnderecoDTO model);
+        bool Update(int id, EnderecoDTO model);
+        bool Delete(int id);
+    }
+}
---
+++ DeliveryCafe.API/DeliveryCafe.API/Services/EnderecoService.cs
@@ -0,0 +1,52 @@
+﻿using AutoMapper;
+using DeliveryCafe.API.Interface.Domain;
+using DeliveryCafe.API.Interface.DTO;
+using DeliveryCafe.API.Models;
+using DeliveryCafe.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace DeliveryCafe.API.Services
+{
+    public class EnderecoService : IEnderecoDTOInterface
+    {
+        private readonly IEnderecoInterface _enderecoContext;
+        private readonly IMapper _mapper;
+        public EnderecoService(IEnderecoInterface endereco, IMapper mapper)
+        {
+            _enderecoContext = endereco;
+            _mapper = mapper;
+        }
+        public EnderecoDTO Insert(EnderecoDTO model)
+        {
+            if (model == null)
+            {
+                return null;
+            }
+            var endereco = _mapper.Map<Endereco>(model);
+            var enderecoRetorno = _enderecoContext.Insert(endereco);
+            return _mapper.Map<EnderecoDTO>(enderecoRetorno);
+        }
+
+        public bool Update(int id, EnderecoDTO model)
+        {
+            if (id == 0 || model == null)
+            {
+                return false;
+            }
+            var endereco = _mapper.Map<Endereco>(model);
+            return _enderecoContext.Update(id, endereco);
+        }
+
+        public bool Delete(int id)
+        {
+            if (id == 0)
+            {
+                return false;
+            }
+            return _enderecoContext.Delete(id);
+        }
+    }
+}
